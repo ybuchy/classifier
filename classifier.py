e751b58b3697ddcda33a524ec6daa4bb0f740f45
ybuchy <63422637+ybuchy@users.noreply.github.com>
2023-06-01 00:05:11 +0200

    wrote backpropagation & acc on test set
    currently 9.8% acc so it does pretty much nothing rn

--- classifier.py
+++ classifier.py
@@ -21,8 +21,10 @@ def relu_derivative(x):
     return 0 if x <= 0 else 1
 
 def sigmoid(x):
-    # currently sigmoid, (use softmax instead?), same for every layer
     return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    return np.exp(x)/((1 + np.exp(x))**2)
 
 def cost(classification, label):
     # ppl use cross entropy, maybe try this first
@@ -40,6 +42,14 @@ def vec(func, iterable):
     return np.fromiter(map(lambda x: func(x), iterable), float)
 
 
+# TODO
+def forward():
+    pass
+
+# TODO
+def backward():
+    pass
+
 def shallow_NN():
     # input layer size: 28^2 (amount of pixels)
     # hidden layer size: try different ones to prevent overfitting (why?) first try: 500 (so about 64%) (try out Grid search(hyperparameter optimization) later on)
@@ -49,46 +59,78 @@ def shallow_NN():
     hl_size = 500
     ol_size = 10
 
-    cur_img = images[0]
-    cur_label = labels[0]
+    print("training model...")
+    weights_in_hid = np.random.rand(hl_size + 1, il_size + 1)
+    weights_hid_out = np.random.rand(ol_size, hl_size + 1)
 
-    # one more for easy bias calculation
-    input_layer = np.array(cur_img + [1])
-    weights_in_hid = np.ones((hl_size + 1, il_size + 1))
-    weights_hid_out = np.ones((ol_size, hl_size + 1))
+    retrain = False
 
-    # forward
-    hidden_layer_old = np.matmul(weights_in_hid, input_layer)
-    hidden_layer = vec(relu, hidden_layer_old)
-    output_layer_old = np.matmul(weights_hid_out, hidden_layer)
-    output_layer = vec(relu, output_layer_old)
+    if retrain:
 
-    # backpropagation TODO
-    """
-    we have C(R(Who*R(Wih*input) as the cost function of our entire net
-    we want derivative wrt Who / Wih using chain rule
-    let Who[1] denote the first row
-    we have (C' o R o A)(Who[1]) where o is function composition and A(x) := x * R(Wih*input)
-    wrt Who[i]: C'(R(Who[i]*R(Wih*input))) * R'(Who[i] * R(Wih*input)) * R(Wih*input)
-    wrt Wih: C'(R(Who*R(Wih[i]*input))) * R'(Who * R(Wih[i]*input)) * Who * R'(Wih[i]*input) * input
-    """
-    Eo = []
-    for i in range(weights_hid_out.shape[0]):
-        n = weights_hid_out[i, :] @ hidden_layer
-        m = 1 if i == cur_label else 0
-        Eo.append((relu(n) - m) * relu_derivative(n))
-    Eo = np.reshape(np.array(Eo), (-1, 1))
-    Eh = []
-    for i in range(weights_in_hid.shape[0]):
-        n = weights_in_hid[i, :] @ output_layer
-        m = 1 if i == cur_label else 0
+        for num in range(len(images)):
+            print(f"cur at {num} of {len(images)}")
+            cur_img = images[num]
+            cur_label = labels[num]
 
-    der_who = Eo @ np.reshape(hidden_layer, (1, -1))
-    der_wih = Eh @ input_layer
+            # one more for easy bias calculation
+            input_layer = np.array(cur_img + [1])
 
-    #update weights
-    weights_hid_out -= learning_rate * der_who
-    weights_in_hid -= learning_rate * der_wih
+            # forward
+            hidden_layer_old = weights_in_hid @ input_layer
+            # using sigmoid rn because otherwise numbers get too big to calculate sigmoid derivative for delta_2
+            hidden_layer = vec(sigmoid, hidden_layer_old)
+            output_layer_old = weights_hid_out @ hidden_layer
+            output_layer = vec(sigmoid, output_layer_old)
+
+            # backpropagation
+            wanted = np.zeros(10)
+            wanted[cur_label] = 1
+            delta_2 = (output_layer - wanted) * vec(sigmoid_derivative, output_layer_old)
+            # TODO changes bias????????
+            der_who = []
+            for err in delta_2:
+                der_who.append(err * hidden_layer)
+            der_who = np.array(der_who)
+            weights_no_bias = weights_hid_out[:, :-1]
+            delta_1 = vec(sigmoid_derivative, hidden_layer[:-1]) * (np.transpose(weights_no_bias) @ delta_2)
+            der_wih = []
+            for err in delta_1:
+                der_wih.append((err * input_layer))
+            der_wih = np.vstack((der_wih, np.zeros(il_size + 1)))
+
+            #update weights
+            weights_hid_out -= learning_rate * der_who
+            weights_in_hid -= learning_rate * der_wih
+
+        print("done training")
+
+        np.save("who.npy", weights_hid_out)
+        np.save("wih.npy", weights_in_hid)
+
+    else:
+        weights_hid_out = np.load("who.npy")
+        weights_in_hid = np.load("wih.npy")
+
+    img_fileinfo = read_image_file(files[2])
+    imgs = img_fileinfo["images"]
+    label_fileinfo = read_label_file(files[3])
+    lbls = label_fileinfo["labels"]
+    print("finished")
+
+    print("testing model...")
+    num_correct = 0
+    for num in range(len(imgs)):
+        input_layer = np.array(imgs[num] + [1])
+        hidden_layer_old = weights_in_hid @ input_layer
+        hidden_layer = vec(sigmoid, hidden_layer_old)
+        output_layer_old = weights_hid_out @ hidden_layer
+        output_layer = vec(sigmoid, output_layer_old)
+        highest = list(output_layer).index(max(output_layer))
+        if highest == lbls[num]:
+            num_correct += 1
+
+    print(num_correct / len(imgs))
+
 
 
 shallow_NN()
