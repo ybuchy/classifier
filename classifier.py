97d28d25c2c7f9d96a3991192a7b342efc7ff14c
ybuchy <63422637+ybuchy@users.noreply.github.com>
2023-06-03 01:00:55 +0200

    continued generalization and refactoring

--- classifier.py
+++ classifier.py
@@ -1,14 +1,22 @@
 from preproc import *
 
+def vec(func, iterable):
+    return np.fromiter(map(lambda x: func(x), iterable), float)
+
+def vec_der(func, iterable):
+    return np.fromiter(map(lambda x: func(x, derivative = True), iterable), float)
+
 class Layer:
     def __init__(self, size, activation, bias=True):
         self.bias = bias
         bias_node = 1 if self.bias else 0
         self.nodes = np.zeros(size + bias_node)
+        self.nodes_pre = np.zeros(size + bias_node)
+        self.activation = activation
 
 
     def set_nodes(self, content):
-        self.nodes = [1] + content if self.bias else content
+        self.nodes = np.array([1] + list(content)) if self.bias else content
 
 
     def __str__(self):
@@ -20,17 +28,52 @@ class Layer:
     def __len__(self):
         return len(self.nodes)
 
+
+    def activate(self):
+        self.nodes_pre = self.nodes.copy()
+        self.nodes = vec(self.activation, self.nodes)
+
+
+    def get_nodes(self):
+        return self.nodes[1:] if self.bias else self.nodes
+
+    def get_nodes_pre(self):
+        return self.nodes_pre[1:] if self.bias else self.nodes_pre
+
+
+def sigmoid(x, derivative=False):
+    if derivative:
+        return np.exp(-x)/((1 + np.exp(-x))**2)
+    return 1/(1 + np.exp(-x))
+
+
+
+def relu(x, derivative=False):
+    if derivative:
+        return 0 if x <= 0 else 1 # non-differentiable in 0 ~> problem?
+    return max(0, x)
+
+
+def cost(classification, wanted, derivative=False): # TODO read about cross entropy
+    dif = classification - wanted
+    if derivative:
+        return 1/len(dif) * dif
+    return 1/(2 * len(dif)) * np.dot(dif, dif)
+
+
+
+
 class Neural_net:
     """
     TODO:
     change activation functions of layers (default sigmoid?)
     currently wants at least one hidden layer, change?
+    try out types?
+    change constructor parameters, confusing rn
+    rename content to nodes as in layer
+    bias 0 all the time?
     """
-    def sigmoid(self, x):
-        return 1/(1 + np.exp(-x))
-
-
-    def __init__(self, num_hid, il_size, ol_size, *hl_sizes):
+    def __init__(self, learning_rate, num_hid, il_size, ol_size, *hl_sizes):
         """
         num_hid: amount of hidden layers
         il_size: amount of input nodes
@@ -41,8 +84,10 @@ class Neural_net:
         if len(hl_sizes) != num_hid:
             raise TypeError("please specify hidden layer sizes")
 
-        self.layers = [Layer(il_size, self.sigmoid)] + list(Layer(size, self.sigmoid) for size in hl_sizes) + [Layer(ol_size, self.sigmoid, bias = False)]
-        self.weights = [np.zeros((hl_sizes[0], il_size))] + list(np.zeros(hl_sizes[i+1], hl_sizes[i]) for i in range(num_hid - 1)) + [np.zeros((ol_size, hl_sizes[-1]))]
+        self.layers = [Layer(il_size, sigmoid)] + list(Layer(size, sigmoid) for size in hl_sizes) + [Layer(ol_size, sigmoid, bias = False)]
+        self.weights = [np.zeros((hl_sizes[0], il_size + 1))] + list(np.zeros((hl_sizes[i+1], hl_sizes[i] + 1)) for i in range(num_hid - 1)) + [np.zeros((ol_size, hl_sizes[-1] + 1))]
+
+        self.learning_rate = learning_rate
 
 
     def set_input_layer(self, nodes):
@@ -52,142 +97,87 @@ class Neural_net:
         self.layers[0].set_nodes(nodes)
 
 
+    def show_output_layer(self):
+        print(self.layers[-1])
+
+
     def forward(self):
-        for layer in self.layers:
-            print(layer)
+        self.layers[0].activate()
         for layer in range(len(self.layers) - 1):
-            self.layers[layer+1].set_nodes(self.layers[layer] @ self.weights[layer])
+            self.layers[layer+1].set_nodes(self.weights[layer] @ self.layers[layer].nodes)
+            self.layers[layer+1].activate()
+
+
+    def backpropagate(self, output):
+        """
+        output: what the output layer should have been
+        """
+        derivatives = []
+        # last layer
+        vec = vec_der(sigmoid, self.layers[-1].get_nodes_pre())
+        cost_grad = cost(self.layers[-1].get_nodes(), output, derivative = True)
+        derivatives.append(np.reshape(vec * cost_grad, (-1, 1)) @ np.reshape(self.layers[-2].get_nodes(), (1, -1)))
+
+        # earlier layers
+        for layer in range(len(self.layers) - 2, 0, -1):
+            # calculate cost gradient wrt activated nodes
+            dc_dz = vec_der(sigmoid, self.layers[layer+1].get_nodes_pre()) * cost_grad
+            cost_grad = np.fromiter((np.dot(self.weights[layer][:, k], dc_dz) for k in range(1, len(self.layers[layer]))), float) # maybe 1 in range wrong
+            vec = vec_der(sigmoid, self.layers[layer].get_nodes())
+            derivatives.append(np.reshape(vec * cost_grad, (-1, 1)) @ np.reshape(self.layers[layer-1].get_nodes(), (1, -1)))
+
+        # update weights
+        for i in range(len(self.weights)):
+            self.weights[i] -= self.learning_rate * np.hstack((np.zeros((self.weights[i].shape[0], 1)), derivatives[len(derivatives) - (i + 1)]))
+
+
+    def save_weights(self):
+        pass #TODO
+    #np.save("who.npy", weights_hid_out)
+    #np.save("wih.npy", weights_in_hid)
 
 
 
-test = Neural_net(1, 1, 1, 1)
-test.set_input_layer(np.array([1]))
-test.forward()
-exit()
-
-files = ["train-images-idx3-ubyte", "train-labels-idx1-ubyte", "t10k-images-idx3-ubyte", "t10k-labels-idx1-ubyte"]
-
-print("loading training data...")
-tr_img_fileinfo = read_image_file(files[0])
-images = tr_img_fileinfo["images"]
-tr_label_fileinfo = read_label_file(files[1])
-labels = tr_label_fileinfo["labels"]
-print("finished")
-
-view_img = lambda num: view_image(images[num], tr_img_fileinfo["num_rows"],
-                                    tr_img_fileinfo["num_cols"])
-#view_img(99)
-#print(labels[99])
-
-def relu(x):
-    return max(0, x)
-
-def relu_derivative(x):
-    return 0 if x <= 0 else 1
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-def sigmoid_derivative(x):
-    return np.exp(-x)/((1 + np.exp(-x))**2)
-
-def cost(classification, label):
-    # ppl use cross entropy, maybe try this first
-    wanted = np.zeros(10)
-    wanted[label] = 1
-    dif = classification - wanted
-    return 1/(2 * len(dif)) * np.dot(dif, dif)
-
-def cost_derivative(classification, label):
-    wanted = np.zeros(10)
-    wanted[label] = 1
-    dif = classification - wanted
-    return 1/len(dif) * dif
-
-def vec(func, iterable):
-    return np.fromiter(map(lambda x: func(x), iterable), float)
-
-
-# TODO
-def forward():
-    pass
-
-# TODO
-def backward():
-    pass
 
 def main():
-    pass
+    files = ["train-images-idx3-ubyte", "train-labels-idx1-ubyte", "t10k-images-idx3-ubyte", "t10k-labels-idx1-ubyte"]
 
+    print("loading training data...")
+    tr_img_fileinfo = read_image_file(files[0])
+    images = tr_img_fileinfo["images"]
+    tr_label_fileinfo = read_label_file(files[1])
+    labels = tr_label_fileinfo["labels"]
+    print("finished")
 
-def shallow_NN():
+    view_img = lambda num: view_image(images[num], tr_img_fileinfo["num_rows"],
+                                      tr_img_fileinfo["num_cols"])
+    #view_img(99)
+    #print(labels[99])
+
     # input layer size: 28^2 (amount of pixels)
-    # hidden layer size: try different ones to prevent overfitting (why?) first try: 500 (so about 64%) (try out Grid search(hyperparameter optimization) later on)
+    # hidden layer size: try different ones to prevent overfitting (why?) first try: 500 (so about 64%) (try out Grid search(hyperparameter optimization) later
     # output layer size: 10 (classification to number)
     learning_rate = .1
     il_size = 28 ** 2
     hl_size = 500
     ol_size = 10
+    net = Neural_net(learning_rate, 1, il_size, ol_size, hl_size)
 
     print("training model...")
-    # CARE that later the layers have numbers in a good range, otherwise derivative of sigmoid function very small -> very slow learning!
-    weights_in_hid = np.zeros((hl_size + 1, il_size + 1))
-    weights_hid_out = np.zeros((ol_size, hl_size + 1))
-
-    print(weights_hid_out.shape)
 
     retrain = True
 
     if retrain:
-
-        for num in range(len(images)):
-            print(f"cur at {num} of {len(images)}")
-            cur_img = images[num]
-            cur_label = labels[num]
-
-            # the [1] will be for bias, same later for hidden layer
-            input_layer = np.array(cur_img + [1])
-
-            # forward
-            hidden_layer_old = weights_in_hid @ input_layer
-            # using sigmoid rn because otherwise numbers get too big to calculate sigmoid derivative for delta_2
-            hidden_layer = vec(relu, hidden_layer_old) + [1]
-            output_layer_old = weights_hid_out @ hidden_layer
-            output_layer = vec(sigmoid, output_layer_old)
-            # backpropagation
+        for (image, label, num) in zip(images, labels, range(len(labels))):
+            net.set_input_layer(image)
+            net.forward()
             wanted = np.zeros(10)
-            wanted[cur_label] = 1
-            c = 1/ol_size * (output_layer - wanted)
-            sd = vec(sigmoid_derivative, output_layer_old)
-            a = np.reshape(c * sd, (-1, 1))
-            der_who = a @ np.reshape(hidden_layer, (1, -1))
-            """
-            for j in range(j_max):
-                for k in range(k_max):
-                    der_who[j, k] = a[j] * hidden_layer[k]
-            """
-            # TODO refactor into function, then try on small NN
-            sd = vec(sigmoid_derivative, hidden_layer_old)
-            B = 1/ol_size * (output_layer - wanted) * vec(relu_derivative, output_layer_old)
-            s = B @ weights_hid_out
-            der_wih = np.reshape(sd * s, (-1, 1)) @ np.reshape(input_layer, (1, -1))
-            """
-            for j in range(j_max):
-                sd = sigmoid_derivative(hidden_layer_old[j])
-                s =  sum(weights_hid_out[l, j] * sigmoid_derivative(output_layer_old[l]) * 1/ol_size * (output_layer[l] - wanted[l]) for l in range(ol_size))
-                for k in range(k_max):
-                    der_wih[j, k] = input_layer[k] * sd[j] * s[j]
-            """
-
-            #update weights
-            weights_hid_out -= learning_rate * der_who
-            weights_in_hid -= learning_rate * der_wih
-
-
+            wanted[label] = 1
+            net.backpropagate(wanted)
+            if num == 500:
+                break
         print("training done")
 
-        np.save("who.npy", weights_hid_out)
-        np.save("wih.npy", weights_in_hid)
 
     else:
         weights_hid_out = np.load("who.npy")
@@ -195,28 +185,30 @@ def shallow_NN():
 
 
     img_fileinfo = read_image_file(files[2])
-    imgs = img_fileinfo["images"]
+    images = img_fileinfo["images"]
     label_fileinfo = read_label_file(files[3])
-    lbls = label_fileinfo["labels"]
+    labels = label_fileinfo["labels"]
     print("finished")
 
     print("testing model...")
     num_correct = 0
-    for num in range(len(imgs)):
-        input_layer = np.array(imgs[num] + [1])
-        hidden_layer_old = weights_in_hid @ input_layer
-        hidden_layer = vec(sigmoid, hidden_layer_old)
-        output_layer_old = weights_hid_out @ hidden_layer
-        output_layer = vec(sigmoid, output_layer_old)
-        print(output_layer)
-        highest = list(output_layer).index(max(output_layer))
-        if highest == lbls[num]:
+    for (image, label, num) in zip(images, labels, range(len(labels))):
+        net.set_input_layer(image)
+        net.forward()
+        output = net.layers[-1].get_nodes()
+        print(output)
+        highest = list(output).index(max(output))
+        if highest == label:
             num_correct += 1
+        if num == 500:
+            break
 
-    print(num_correct / len(imgs))
+    print(num_correct / len(images))
 
 
 
 
 if __name__ == "__main__":
     main()
+
+# CARE that later the layers have numbers in a good range, otherwise derivative of sigmoid function very small -> very slow learning!
