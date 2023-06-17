fixed backprop and added validation set usage when training

cl_module/classifier.py
@@ -99,7 +99,7 @@ def cat_cross_entropy(classification, wanted):
 
 
 def cat_cross_entropy_batchtensor(classification, wanted):
-    return -np.diag(wanted @ np.log(classification).T)
+    return -np.diag(wanted.T @ np.log(classification))
 
 
 class NN_classifier:
@@ -241,9 +241,24 @@ class NN_classifier:
     def mini_batch_gd(self, inp, outp):
         self.forward(inp)
         self.backpropagate(outp)
-        loss = cat_cross_entropy(self.layers[-1].get_units(), outp)
+        output_layer = self.layers[-1].get_units()
+        loss = cat_cross_entropy_batchtensor(output_layer, outp)
+
         #self.update_weights_and_biases()
         return sum(loss) / len(loss)
+
+    def check_acc(self, ch_set):
+        im_tensor, op_tensor = ch_set
+        self.forward(im_tensor)
+        output_layer = self.layers[-1].get_units()
+        output_layer.shape
+        classifications = np.argmax(output_layer, axis=0)
+        labels = np.argmax(op_tensor, axis=0)
+        correct = 0
+        for classification, label in zip(classifications, labels):
+            if classification == label:
+                correct += 1
+        return correct / len(classifications)
 
     def save_weights(self):
         pass  # TODO

cl_module/preproc.py
@@ -125,28 +125,28 @@ class Preproc:
             tr_images, tr_labels, batch_size)
         return list(zip(img_batches, label_batches))
 
-    def get_validation_set(self, batch_size):
+    def get_validation_set(self):
 #    def get_validation_set(self, batch_size: int) -> list[tuple[image_batch, label_batch]]:
         if not self.loaded:
             raise AttributeError("mnist data not in memory yet, use load() first")
         val_size = int(1/5 * self.tr_set_file_info["num_images"])
-        test_images = np.array(self.tr_set_file_info["images"])
-        test_labels = np.array(self.tr_label_file_info["labels"])
-        val_images = test_images[-val_size:]
-        val_labels = test_labels[-val_size:]
+        tr_images = np.array(self.tr_set_file_info["images"])
+        tr_labels = np.array(self.tr_label_file_info["labels"])
+        val_images = tr_images[-val_size:]
+        val_labels = tr_labels[-val_size:]
         img_batches, label_batches = self.generate_batches(
-            val_images, val_labels, batch_size)
-        return list(zip(img_batches, label_batches))
+            val_images, val_labels, val_size)
+        return (img_batches[0], label_batches[0])
 
-    def get_test_set(self, batch_size):
+    def get_test_set(self):
         #def get_test_set(self, batch_size: int) -> list[tuple[image_batch, label_batch]]:
         if not self.loaded:
             raise AttributeError("mnist data not in memory yet, use load() first")
         test_images = np.array(self.test_set_file_info["images"])
         test_labels = np.array(self.test_label_file_info["labels"])
         img_batches, label_batches = self.generate_batches(
-            test_images, test_labels, batch_size)
-        return list(zip(img_batches, label_batches))
+            test_images, test_labels, len(test_images))
+        return (img_batches[0], label_batches[0])
 
     def load(self) -> None:
 

test/test_classifier.py
@@ -9,8 +9,11 @@ class TestNN(unittest.TestCase):
         ind_a, ind_b = np.random.randint(0, 50), np.random.randint(0, 50)
         out_a, out_b = np.zeros(50), np.zeros(50)
         out_a[ind_a], out_b[ind_b] = 1, 1
-        cl_tensor = np.vstack((inp_a, inp_b))
-        lb_tensor = np.vstack((out_a, out_b))
+        cl_tensor = np.hstack((np.reshape(inp_a, (-1, 1)),
+                               np.reshape(inp_b, (-1, 1))))
+        print(cl_tensor.shape)
+        lb_tensor = np.hstack((np.reshape(out_a, (-1, 1)),
+                               np.reshape(out_b, (-1, 1))))
 
         a = cat_cross_entropy(inp_a, out_a)
         b = cat_cross_entropy(inp_b, out_b)
@@ -55,7 +58,7 @@ class TestNN(unittest.TestCase):
             weight_bias_matrix = np.hstack((bias, weights))
             net.weights[i] = weight_bias_matrix
         # get random inputs
-        inp1, inp2 = 1/50 * torch.tensor(range(50)), 1/50 * torch.tensor(range(50,0,-1))
+        inp1, inp2 = torch.rand(50), torch.rand(50)
         # classifier input batch matrix
         net_tensor = np.hstack((np.reshape(inp1.detach().numpy(), (-1, 1)),
                                 np.reshape(inp2.detach().numpy(), (-1, 1))))
@@ -69,6 +72,8 @@ class TestNN(unittest.TestCase):
         np.testing.assert_allclose(clas_fw[:,0], torch_fw1, rtol=1e-5, atol=1e-6)
         np.testing.assert_allclose(clas_fw[:,1], torch_fw2, rtol=1e-5, atol=1e-6)
 
+    def test_batch_backprop_relu_softmax(self):
+        pass
 
 
 if __name__ == "__main__":